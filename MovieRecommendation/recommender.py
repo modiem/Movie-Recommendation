7f55addc74f9f6202dec4ebd6335f9a35168dc79
Mo Yang <mo_yang@foxmail.com>
2021-02-17 22:19:23 +0100

    small changes

--- MovieRecommendation/recommender.py
+++ MovieRecommendation/recommender.py
@@ -5,17 +5,17 @@ from MovieRecommendation.gcp import download_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 
 class Recommender(object):
-    def __init__(self, sample, benchmark="metadata", hybrid=True, n_movies=3):
+    def __init__(self, sample, basis="metadata", hybrid=True, n_movies=3):
         self.sample = sample
-        self.benchmark = benchmark
+        self.basis = basis
         self.hybrid=hybrid
         self.n_movies = n_movies
 
-    def similarity_df(self, benchmark):
-        matrix = download_matrix(f"{benchmark}")
+    def similarity_df(self, basis):
+        matrix = download_matrix(f"{basis}")
         v=np.array(matrix.loc[self.sample]).reshape(1, -1)
         sim =  cosine_similarity(matrix, v).reshape(-1)
-        df = pd.DataFrame({f"{benchmark}_similarity": sim}, index=matrix.index.tolist())
+        df = pd.DataFrame({f"{basis}_similarity": sim}, index=matrix.index.tolist())
         return df
 
     
@@ -31,9 +31,9 @@ class Recommender(object):
             recommendations = df[1:self.n_movies+1].hybrid_similarity
         
         else:
-            df=self.similarity_df(self.benchmark).sort_values(by=f"{self.benchmark}_similarity", ascending=False)   
+            df=self.similarity_df(self.basis).sort_values(by=f"{self.basis}_similarity", ascending=False)   
             print("tto")
-            recommendations = df[1:self.n_movies+1][f"{self.benchmark}_similarity"]
+            recommendations = df[1:self.n_movies+1][f"{self.basis}_similarity"]
         
         return recommendations
         
